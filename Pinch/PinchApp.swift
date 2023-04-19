498939e85fd5825bdf813d6ab5069a6455df48f8
Gurleen Kaur <gurleen.kaur101010@gmail.com>
2023-04-18 20:59:56 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Gurleen Kaur on 2023-04-18.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
