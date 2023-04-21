163f432586e4b11b8edec4b7fc849b967ae4294c
Gurleen Kaur <gurleen.kaur101010@gmail.com>
2023-04-21 03:37:51 -0400

    Drag gestured added and corrected it so it can return to normal state after zoom as well

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,6 +11,16 @@ struct ContentView: View {
     
     @State private var isAnimation: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    
+    func resetImageState(){
+        return withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
     var body: some View {
         NavigationView{
             ZStack{
@@ -22,6 +32,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12 , x: 2, y: 2)
                     .opacity(isAnimation ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimation)
                     .scaleEffect(imageScale)
                     .onTapGesture(count: 2, perform: {
@@ -30,11 +41,22 @@ struct ContentView: View {
                                 imageScale = 6
                             }
                         } else {
-                            withAnimation(.spring()){
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                    .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 1)){
+                                imageOffset = value.translation
+                            }
+                        }
+                        .onEnded{ _ in
+                            if imageScale <= 1{
+                               resetImageState()
+                            }
+                        }
+                    )
             }//: zstack
             .navigationTitle("Pinch and Zoom")
             .navigationBarTitleDisplayMode(.inline)
