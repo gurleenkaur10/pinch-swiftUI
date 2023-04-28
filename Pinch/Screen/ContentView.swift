fce91f7a671e69e832ebd34904423047c8f4f06a
Gurleen Kaur <gurleen.kaur101010@gmail.com>
2023-04-27 22:49:56 -0400

    Added two feature one is the info panel and shows insights about zoom and image offsets it will be visible after long press the circle icon second is the scale up and down buttons on the bottom of the image.

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             ZStack{
+                Color.clear
            // MARK: - Page Image
                 Image("magazine-front-cover")
                     .resizable()
@@ -63,6 +64,61 @@ struct ContentView: View {
             .onAppear(perform: {
                 isAnimation = true
             })
+            //MARK: - Info Panel
+            .overlay(
+            InfoPanelView(scale: imageScale, offset: imageOffset)
+                .padding(.horizontal)
+                .padding(.top, 30)
+            , alignment: .top
+            )
+            
+            //MARK: - Controls
+            .overlay(
+                Group{
+                    HStack{
+                        // Scale Down
+                        Button{
+                            withAnimation(.spring()){
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    if imageScale <= 1{
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        
+                        // Reset
+                        Button{
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        // Scale Up
+                        Button{
+                            withAnimation(.spring()){
+                                if imageScale < 5{
+                                    imageScale += 1
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                        
+                    }//MARK: - End of control
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimation ? 1 : 0)
+                }
+                    .padding(.bottom, 30)
+                ,alignment: .bottom
+            )
         }// NavigationView
         .navigationViewStyle(.stack)
     }
